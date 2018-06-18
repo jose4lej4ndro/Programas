82f9b2f5eef389fea43d76ce16798debabd1099d
jose4lej4ndro <jose4lej4ndro@gmail.com>
2018-06-18 19:16:54 -0400

    programa que muestra palabras ingresadas por el usuario

---
+++ nivel_2/palabras.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int v1;
+    char palabras[20];
+    cout<<"porfavor ingrese la palabra a mostrar"<<endl;
+    cin>>palabras;
+    cout<<palabras<<endl;
+    return 0;
+}
