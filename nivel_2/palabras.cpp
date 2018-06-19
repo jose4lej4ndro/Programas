2f0c036fe92e7fa3408a32bee669d2a495388881
jose4lej4ndro <jose4lej4ndro@gmail.com>
2018-06-19 16:26:14 -0400

    programa que muestra la ultima letra de una palabra

--- nivel_2/palabras.cpp
+++ nivel_2/palabras.cpp
@@ -3,9 +3,9 @@ using namespace std;
 int main()
 {
     int v1;
-    char palabras[20];
+    char palabras[20]  ;
     cout<<"porfavor ingrese la palabra a mostrar"<<endl;
     cin>>palabras;
-    cout<<palabras<<endl;
+    cout<<palabras[4]<<endl;
     return 0;
 }
---
+++ nivel_2/ultimaletra.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i,resultado;
+    char palabras[20];
+    cout<<"porfavor ingrese la palabra a invertir"<<endl;
+    cin>>palabras; 
+    for( i=0 ; i<='\0' ; i++)
+    {  
+         
+        cout<<"el resultado es "<<palabras[i]<<endl;  
+    
+    }   
+       
+   
+     return 0;
+}
