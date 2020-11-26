ebc50f630e1010f9e9e05832c8dcc6378a710e26
jose4lej4ndro <jose4lej4ndro@gmail.com>
2020-11-25 23:34:21 -0400

    Agregado nuevo programa y solucionado 1 error

--- Python/ejemplo.py
+++ Python/ejemplo.py
@@ -11,7 +11,7 @@ elif opc == 2 :
     num1 = input("ingresa el primer numero: ")
     num2 = input("ingresa el segundo numero: ")
 
-    res = float(num1) - float(num2)
+    res = int(num1) - float(num2)
     print(res)
 
 elif opc == 3 :
@@ -23,9 +23,14 @@ elif opc == 3 :
 
 elif opc == 4 :
     num1 = input("ingresa el primer numero: ")
-    num2 = input("ingresa el segundo numero: ")
+    num2 = input("ingresa el segundo numero: ") 
+    
+    if num2 == 0 :
+        print("Error, no se puede dividir entre cero")
+    
+    else:
+        res = float(num1) / float(num2)
+        print(res)
 
-    res = float(num1) / float(num2)
-    print(res)
 else:
 	print("Por favor ingresa una opcion correcta")
