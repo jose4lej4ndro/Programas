ef0283dbe0ca3724b53a832d7b2a5e592fcadc8f
jose4lej4ndro <jose4lej4ndro@gmail.com>
2020-11-25 22:59:29 -0400

    Incluidos nuevos programas de Python

---
+++ Python/ejemplo.py
@@ -0,0 +1,31 @@
+opc = input(" elije una opcion: 1 para sumar, 2 para restar, 3 para multiplcar y 4 para dividir:  ")
+
+if opc == 1 :
+   num1 = input("ingresa el primer numero: ")
+   num2 = input("ingresa el segundo numero: ")
+
+   res = float(num1) + float(num2)
+   print(res)
+
+elif opc == 2 :
+    num1 = input("ingresa el primer numero: ")
+    num2 = input("ingresa el segundo numero: ")
+
+    res = float(num1) - float(num2)
+    print(res)
+
+elif opc == 3 :
+    num1 = input("ingresa el primer numero: ")
+    num2 = input("ingresa el segundo numero: ")
+
+    res = float(num1) * float(num2)
+    print(res)
+
+elif opc == 4 :
+    num1 = input("ingresa el primer numero: ")
+    num2 = input("ingresa el segundo numero: ")
+
+    res = float(num1) / float(num2)
+    print(res)
+else:
+	print("Por favor ingresa una opcion correcta")
